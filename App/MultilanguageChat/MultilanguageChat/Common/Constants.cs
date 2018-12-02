9ee2ba0fb6b2d7b320159a7b4b0f9e9712b542fc
Marco Minerva <marco.minerva@gmail.com>
2018-12-02 15:14:59 +0100

    Add client app project

---
+++ App/MultilanguageChat/MultilanguageChat/Common/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultilanguageChat.Common
+{
+    public static class Constants
+    {
+        public const string TranslatorSubscriptionKey = "";
+        public const string SpeechRegion = "";
+        public const string SpeechSubscriptionKey = "";
+    }
+}
---
+++ App/MultilanguageChat/MultilanguageChat/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultilanguageChat.ViewModels
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+
+        }
+
+        //public MainViewModel MainViewModel => SimpleIoc.Default.GetInstance<MainViewModel>();
+    }
+}
